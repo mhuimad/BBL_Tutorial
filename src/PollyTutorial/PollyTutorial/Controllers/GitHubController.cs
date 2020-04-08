65b0755ff531afb659145c41603c0c26eeb80bc5
Mohamed HUIMAD <Mohamed.HUIMAD@s-money.net>
2020-04-08 23:46:19 +0200

    PollyTuto -  init tuto

---
+++ src/PollyTutorial/PollyTutorial/Controllers/GitHubController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PollyTutorial.Contracts;
+
+namespace PollyTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        private readonly IGitHubService _gitHubService;
+
+        public GitHubController(IGitHubService gitHubService)
+        {
+            _gitHubService = gitHubService;
+        }
+
+
+
+        // GET: api/GitHub/5
+        [HttpGet("users/{userName}", Name = "Get")]
+        public async Task<IActionResult> GetUserByUserNameAsync(string userName)
+        {
+            var user = await _gitHubService.GetUserByUserNameAsync(userName);
+            return user != null ? (IActionResult)Ok(user) : NotFound();
+        }
+
+    }
+
+
+}
---
+++ src/PollyTutorial/PollyTutorial/Services/GitHubService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PollyTutorial.Dto;
+using PollyTutorial.Contracts;
+
+namespace PollyTutorial.Services
+{
+    public class GitHubService : IGitHubService
+    {
+        private readonly IHttpClientFactory _httpFactory;
+        private static readonly Random Random = new Random();
+        private const int MaxRetries = 3;
+
+        public GitHubService(IHttpClientFactory httpFactory)
+        {
+            _httpFactory = httpFactory;
+        }
+
+        public async Task<User> GetUserByUserNameAsync(string userName)
+        {
+            var client = _httpFactory.CreateClient("GitHub");
+            var retriesLeft = MaxRetries;
+            User user = null;
+            while (retriesLeft > 0)
+            {
+                try
+                {
+                    if (Random.Next(1, 3) == 1)
+                        throw new HttpRequestException("Fake request exception ");
+
+                    var result = await client.GetAsync($"/users/{userName}");
+                    if (result.StatusCode == System.Net.HttpStatusCode.NotFound)
+                        break;
+
+                    var resultString = await result.Content.ReadAsStringAsync();
+                    user = JsonConvert.DeserializeObject<User>(resultString);
+                    break;
+                }
+                catch (HttpRequestException)
+                {
+                    retriesLeft--;
+                    if (retriesLeft == 0)
+                        throw;
+                }
+
+
+            }
+
+            return user;
+        }
+
+
+    }
+}
